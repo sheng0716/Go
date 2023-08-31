598c96d15287543463565d04bc08295ac4d9b912
Jie <109793139+sheng0716@users.noreply.github.com>
2023-08-31 17:05:07 +0800

    Update add.go

--- math/add.go
+++ math/add.go
@@ -1,14 +1,8 @@
-package main
+package math
 
 import "fmt"
 
 // Function to calculate the sum of two integers
 func add(a, b int) int {
     return a + b
-}
-
-func main() {
-    // Call the add function and print the result
-    result := add(5, 3)
-    fmt.Println("The sum is:", result)
 }
